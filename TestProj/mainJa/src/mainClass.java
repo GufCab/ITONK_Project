4af563647275bee7bcf4b57882e290bc2a4d87f4
chr22 <chrmaui@gmail.com>
2014-05-06 15:25:54 +0200

    changed qos writer

--- TestProj/mainJa/src/mainClass.java
+++ TestProj/mainJa/src/mainClass.java
@@ -1,8 +1,6 @@
 import com.rti.dds.domain.DomainParticipant;
 import com.rti.dds.domain.DomainParticipantFactory;
-import com.rti.dds.infrastructure.DurabilityQosPolicyKind;
-import com.rti.dds.infrastructure.Duration_t;
-import com.rti.dds.infrastructure.HistoryQosPolicyKind;
+import com.rti.dds.infrastructure.*;
 import com.rti.dds.publication.DataWriterQos;
 import com.rti.dds.publication.Publisher;
 import com.rti.dds.topic.TopicQos;
@@ -22,23 +20,31 @@ public class mainClass {
 
             TopicQos topicQos = DomainParticipant.TOPIC_QOS_DEFAULT;
             DataWriterQos dataWriterQos = Publisher.DATAWRITER_QOS_DEFAULT;
-            Duration_t duration_t = new Duration_t(100000,100000);
+            //Duration_t duration_t = new Duration_t(100000,100000);
 
-            topicQos.lifespan.duration.add(duration_t);
+            topicQos.lifespan.duration.add(new Duration_t(Duration_t.DURATION_INFINITE_SEC, Duration_t.DURATION_INFINITY_NSEC));
 
-            dataWriterQos.lifespan.duration.add(duration_t);
+            dataWriterQos.lifespan.duration.add(new Duration_t(Duration_t.DURATION_INFINITE_SEC, Duration_t.DURATION_INFINITY_NSEC));
             dataWriterQos.durability.kind = DurabilityQosPolicyKind.TRANSIENT_DURABILITY_QOS;
             dataWriterQos.history.kind = HistoryQosPolicyKind.KEEP_LAST_HISTORY_QOS;
             dataWriterQos.history.depth = 100;
+            dataWriterQos.reliability.kind = ReliabilityQosPolicyKind.RELIABLE_RELIABILITY_QOS;
+            dataWriterQos.ownership.kind = OwnershipQosPolicyKind.SHARED_OWNERSHIP_QOS;
+            dataWriterQos.liveliness.kind = LivelinessQosPolicyKind.AUTOMATIC_LIVELINESS_QOS;
+            dataWriterQos.liveliness.lease_duration.add(new Duration_t(Duration_t.DURATION_INFINITE_SEC, Duration_t.DURATION_INFINITY_NSEC));
+            
 
-            OfficePublisher gossipper = new OfficePublisher(GOSSIP_TOPIC, DomainParticipant.TOPIC_QOS_DEFAULT, DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, dataWriterQos);
+            //dataWriterQos.batch.enable = true;
+            //dataWriterQos.batch.max_flush_delay =
+
+            OfficePublisher gossipPub = new OfficePublisher(GOSSIP_TOPIC, DomainParticipant.TOPIC_QOS_DEFAULT, DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, dataWriterQos);
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
             while (true) {
                 System.out.print("Please type a message> ");
                 try {
                     String toWrite = reader.readLine();
-                    gossipper.PublishMessage(toWrite);
+                    gossipPub.PublishMessage(toWrite);
                 } catch (IOException e)  {
                     System.out.println("Something went.. wrong..?");
                 }
