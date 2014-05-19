20cfd50078190ff70842dd02c58e99e85d71d450
GufCab <nikolaj@donald.dk>
2014-05-19 09:26:29 -0700

    Added tests for more pc's

---
+++ 03_RMI/Code/TestClient/src/example/hello/Client.java
@@ -0,0 +1,28 @@
+package example.hello;
+
+/**
+ * Created by guf on 5/19/14.
+ */
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    private Client() {}
+
+    public static void main(String[] args) {
+        String host = (args.length < 1) ? null : args[0];
+
+        try {
+            Registry registry = LocateRegistry.getRegistry("rmi://192.168.147.135");
+            Hello stub = (Hello) registry.lookup("Hello");
+            String response = stub.sayHello();
+            System.out.println("response: " + response);
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+
+        }
+    }
+}
