57a3e912dd9ec96d7f6369f2eb925771fac236fc
chr22 <chrmaui@gmail.com>
2014-05-20 14:36:07 +0200

    added property

--- 03_RMI/Code/TestRmi/src/example/hello/Main.java
+++ 03_RMI/Code/TestRmi/src/example/hello/Main.java
@@ -7,13 +7,13 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String args[]) {
-
+        System.setProperty("java.rmi.server.hostname", "192.168.204.130");
         ArrayList<Node> nodes = new ArrayList<Node>();
 
         for(String arg: args) {
             nodes.add(new Node(arg, Integer.parseInt(arg), false));
         }
-        
+
         nodes.get(0).CallGloriousLeader();
 
         while(true){}
